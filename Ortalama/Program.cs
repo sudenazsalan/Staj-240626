8003a0bd426234ecd2dee8cf8a6ee78519d2c922
sudenazsalan <sudemuqsalan@gmail.com>
2026-06-26 09:11:41 +0300

    dd #
    Not ortalaması hesaplama programı eklendi
    
    Program.cs'deki "Hello, World!" kodu kaldırıldı. Yerine, kullanıcıdan ders sayısı ve notlarını alıp ortalama hesaplayan, geçme/kalma durumunu ekrana yazdıran yeni bir program eklendi. Program sonunda çıkış için tuşa basılması isteniyor.

--- Ortalama/Program.cs
+++ Ortalama/Program.cs
@@ -1,2 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
-degisiklik
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("NOT ORTALAMASI HESAPLAMA");
+
+        Console.Write("Kaç ders girilecek? : ");
+        int dersSayisi = Convert.ToInt32(Console.ReadLine());
+
+        int toplamNot = 0;
+
+        for (int i = 1; i <= dersSayisi; i++)
+        {
+            Console.Write(i + ". ders notunu giriniz: ");
+            int not = Convert.ToInt32(Console.ReadLine());
+
+            toplamNot += not;
+        }
+
+        double ortalama = (double)toplamNot / dersSayisi;
+
+        Console.WriteLine("***");
+        Console.WriteLine("Ortalamanız: " + ortalama);
+
+        if (ortalama >= 50)
+        {
+            Console.WriteLine(" Sonuc: GEÇTİ");
+        }
+        else
+        {
+            Console.WriteLine("Sonuc: KALDI");
+        }
+
+        Console.WriteLine("***");
+        Console.WriteLine("Çıkmak için bir tuşa bas...");
+        Console.ReadKey();
+    }
+}
