981a67fd1e08a6804aa223db372c02000d9611e6
sudenazsalan <sudemuqsalan@gmail.com>
2026-07-03 09:00:00 +0300

    sayı tahmin

---
+++ sayı tahmin/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SayiTahminOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int tutulanSayi = rnd.Next(1, 101);
+            int tahmin = 0;
+            int hak = 7;
+
+            Console.WriteLine("SAYI TAHMİN");
+            Console.WriteLine("1 ile 100 arasında bir sayı tut.");
+            Console.WriteLine("Toplam 7 tahmin hakkın var.");
+
+            while (hak > 0)
+            {
+                Console.Write("\nTahminini gir: ");
+
+                if (!int.TryParse(Console.ReadLine(), out tahmin))
+                {
+                    Console.WriteLine("Lütfen geçerli bir sayı gir.");
+                    continue;
+                }
+
+                if (tahmin == tutulanSayi)
+                {
+                    Console.WriteLine("Tebrikler ederim.");
+                    break;
+                }
+                else if (tahmin < tutulanSayi)
+                {
+                    Console.WriteLine("Daha büyük bir sayı dene.");
+                }
+                else
+                {
+                    Console.WriteLine("Daha küçük bir sayı dene.");
+                }
+
+                hak--;
+
+                if (hak > 0)
+                {
+                    Console.WriteLine("Kalan hak: " + hak);
+                }
+            }
+
+            if (tahmin != tutulanSayi)
+            {
+                Console.WriteLine("\nTahmin hakların bitti.");
+                Console.WriteLine("Tutulan sayı: " + tutulanSayi);
+            }
+
+            Console.WriteLine("\nOyundan çıkmak için bir tuşa bas.");
+            Console.ReadKey();
+        }
+    }
+}
